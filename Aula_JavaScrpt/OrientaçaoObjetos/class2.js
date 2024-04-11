30f535c1e5ef109996d197c1aff0be12973af8b0
Maycon Douglas <maycondouglasniculau@gmail.com>
2024-04-11 14:44:57 -0300

    PraticandoLogica

--- Aula_JavaScrpt/OrientaçaoObjetos/class2.js
+++ Aula_JavaScrpt/OrientaçaoObjetos/class2.js
@@ -1,17 +1,15 @@
 
-
+/*
 class Card{
     constructor(name , speed) {
         this.name = name 
         this.speed = speed
     }
 
-
     acelerar() {
         if (this.speed >= 100) return;
         this.speed++
     }
-
 
     freiar() {
         if (this.velocidade <= 0) return;
@@ -19,3 +17,38 @@ class Card{
     }
 
 }
+*/
+
+
+
+class DispositoEletrinico {
+    constructor(name) {
+        this.name = name
+        this.ligado = false;
+    }
+
+    ligar() {
+        if (this.ligado == true){
+            console.log(`O ${this.name} esta ligador`)
+            return;
+        }
+
+        this.ligado = true;
+    }
+
+    desligar() {
+        if (!this.ligado){
+            console.log(`O ${this.name} ja esta desligador`)
+            return;
+        }
+
+        this.ligado = false;
+    }
+
+
+}
+
+const d1 = new DispositoEletrinico('Smartphone');
+d1.desligar()
+
+console.log(d1)
