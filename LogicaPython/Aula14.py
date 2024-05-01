d7b4a000d652c93bcbd5d12312486a4d54dd43c2
Maycon Douglas <maycondouglasniculau@gmail.com>
2024-05-01 18:22:12 -0300

    PraticandoLogica

---
+++ LogicaPython/Aula14.py
@@ -0,0 +1,17 @@
+
+    
+
+def perso(name ,surname):
+    name = name 
+    surname = surname
+
+    return f"Hello! My name is {name} {surname}"
+
+
+p1 = perso("Maycon", "douglas")
+p2 = perso("Beatriz", "serena")
+
+print(p1)
+print("---" * 10)
+print(p2)
+
