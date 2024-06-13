c0a9fc9e2e378807bcb3fb868349c0fee410de26
Maycon <maycondouglasniculau@gmail.com>
2024-06-13 14:13:57 -0300

    PraticandoLogica

--- Logica python 2/Banco:Escola:Hospital/Banco.py
+++ Logica python 2/Banco:Escola:Hospital/Banco.py
@@ -1,2 +1,24 @@
-class Pessoa:
-    pass
+class Conta:
+    
+
+    def __init__(self , numero , titular , saldo):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+    
+    def extrato(self):
+        print(f"Saldo: R${self.saldo}")
+    
+    def depositar(self, valor):
+        self.saldo += valor
+    
+    def sacar(self, valor):
+        self.saldo -= valor
+ 
+# Instancia da Classe Conta
+
+p1 = Conta(1232, "Nicolau", 2000)   
+print(p1.titular)
+p1.extrato() #Saldo: R$2000
+p1.depositar(500)
+p1.extrato() #Saldo: R$2500
--- Logica python 2/Banco:Escola:Hospital/Hospital.py
+++ Logica python 2/Banco:Escola:Hospital/Hospital.py
@@ -1,2 +1,27 @@
-class Pessoa:
+from datetime import date
+
+
+
+class Paciente:
+    
+
+    def __init__(self , name , idade , cpf , email):
+        self.name = name
+        self.idade = idade
+        self.cpf = cpf
+        self.email = email
+    
+    @classmethod
+    def idadeAnoNascimento(cls , name , anoNascimento, cpf , email):
+        idade = date.today().year - anoNascimento
+        return Paciente(name , idade , cpf , email)
+
+class Medico:
     pass
+
+
+
+
+
+
+
